81a0ff60888f666b33a1cfaecef622d0e832e3b8
qte <>
2025-05-20 16:26:03 +0300

    fix useSignalConnect not beeing exported

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export * from "#src/hook/dom/attribute.js"
 
 export * from "#src/hook/signal/value.js"
 export * from "#src/hook/signal/output.js"
+export * from "#src/hook/signal/connect.js"
 export * from "#src/hook/signal/effect.js"
 export * from "#src/hook/signal/event_deps.js"
